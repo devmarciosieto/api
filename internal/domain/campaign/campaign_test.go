4fafa3e7c8955d9c1bbb3d6cfc3a9293f02be671
marcio sieto <mmmsieto@hotmail.com>
2023-11-02 20:25:09 -0300

    update

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,26 +1,40 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCampaign(t *testing.T) {
+
+	assert := assert.New(t)
+
 	name := "Nome Campaign"
 	content := "Conteúdo Content"
 	contacts := []string{"email01@gmail.com", "email02@gmail.com"}
 
 	campaign := NewCampaign(name, content, contacts)
 
-	if campaign.ID != "1" {
-		t.Errorf("Expected id 1, but got %s", campaign.ID)
-	} else if campaign.Name != name {
-		t.Errorf("Expected name %s, but got %s", name, campaign.Name)
-	} else if campaign.Content != content {
-		t.Errorf("Expected content %s, but got %s", content, campaign.Content)
-	} else if len(campaign.Contacts) != len(contacts) {
-		t.Errorf("Expected %d contacts, but got %d", len(contacts), len(campaign.Contacts))
-	} else if campaign.Contacts[0].Email != contacts[0] {
-		t.Errorf("Expected contact %s, but got %s", contacts[0], campaign.Contacts[0])
-	} else if campaign.Contacts[1].Email != contacts[1] {
-		t.Errorf("Expected contact %s, but got %s", contacts[1], campaign.Contacts[1])
-	}
+	// if campaign.ID != "1" {
+	// 	t.Errorf("Expected id 1, but got %s", campaign.ID)
+	// } else if campaign.Name != name {
+	// 	t.Errorf("Expected name %s, but got %s", name, campaign.Name)
+	// } else if campaign.Content != content {
+	// 	t.Errorf("Expected content %s, but got %s", content, campaign.Content)
+	// } else if len(campaign.Contacts) != len(contacts) {
+	// 	t.Errorf("Expected %d contacts, but got %d", len(contacts), len(campaign.Contacts))
+	// } else if campaign.Contacts[0].Email != contacts[0] {
+	// 	t.Errorf("Expected contact %s, but got %s", contacts[0], campaign.Contacts[0])
+	// } else if campaign.Contacts[1].Email != contacts[1] {
+	// 	t.Errorf("Expected contact %s, but got %s", contacts[1], campaign.Contacts[1])
+	// }
+
+	assert.Equal("1", campaign.ID)
+	assert.Equal(name, campaign.Name)
+	assert.Equal(content, campaign.Content)
+	assert.Equal(len(contacts), len(campaign.Contacts))
+	assert.Equal(contacts[0], campaign.Contacts[0].Email)
+	assert.Equal(contacts[1], campaign.Contacts[1].Email)
 
 }
