c435b505c842ad49417cf1c02d5fe2012b96990c
marcio sieto <mmmsieto@hotmail.com>
2023-11-03 10:21:03 -0300

    update

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/devmarciosieto/api/internal/contract"
@@ -17,17 +18,19 @@ func (m *MockRepository) Save(campaign *Campaign) error {
 	return args.Error(0)
 }
 
-func Test_Create_Save_Campaign(t *testing.T) {
-
-	assert := assert.New(t)
-
-	newCampaign := contract.NewCampaignDto{
+var (
+	newCampaign = contract.NewCampaignDto{
 		Name:    "Test Name",
 		Content: "Test body",
 		Emails:  []string{"email1@gmail.com", "email2gmail.com"},
 	}
+	repositoryMock = new(MockRepository)
+	service        = Service{Repository: repositoryMock}
+)
 
-	repositoryMock := new(MockRepository)
+func Test_Create_Save_Campaign(t *testing.T) {
+
+	assert := assert.New(t)
 
 	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
 
@@ -48,12 +51,34 @@ func Test_Create_Save_Campaign(t *testing.T) {
 		return true
 	})).Return(nil)
 
-	service := Service{Repository: repositoryMock}
-
 	id, err := service.Create(newCampaign)
 
 	assert.NotNil(id)
 	assert.Nil(err)
 
 	repositoryMock.AssertExpectations(t)
+}
+
+func Test_Create_ValidateDomainError(t *testing.T) {
+
+	assert := assert.New(t)
+
+	newCampaign.Name = ""
+	_, err := service.Create(newCampaign)
+
+	assert.NotNil(err)
+	assert.Equal("Name is required", err.Error())
+}
+
+func Test_Create_ValidateRepositorySave(t *testing.T) {
+
+	assert := assert.New(t)
+
+	repositoryMock.On("Save", mock.Anything).Return(errors.New("Error saving campaign"))
+
+	_, err := service.Create(newCampaign)
+
+	assert.NotNil(err)
+	assert.Equal("Error saving campaign", err.Error())
+
 }
