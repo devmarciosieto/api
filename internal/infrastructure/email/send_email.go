ef8ff7a7b18e35e0bca082d37648fb875405e014
marcio sieto <mmmsieto@hotmail.com>
2023-11-12 16:01:50 -0300

    update

--- internal/infrastructure/email/send_email.go
+++ internal/infrastructure/email/send_email.go
@@ -5,6 +5,7 @@ import (
 	"github.com/devmarciosieto/api/internal/domain/campaign"
 	"gopkg.in/gomail.v2"
 	"os"
+	"time"
 )
 
 func SendEmail(campaign *campaign.Campaign) error {
@@ -16,7 +17,10 @@ func SendEmail(campaign *campaign.Campaign) error {
 		emails = append(emails, contact.Email)
 	}
 
+	start := time.Now()
 	d := gomail.NewDialer(os.Getenv("EMAIL_SMTP"), 587, os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"))
+	duration := time.Since(start)
+	fmt.Println("Dialer created in", duration)
 
 	m := gomail.NewMessage()
 	m.SetHeader("From", os.Getenv("EMAIL_FROM"))
@@ -24,5 +28,10 @@ func SendEmail(campaign *campaign.Campaign) error {
 	m.SetHeader("Subject", campaign.Name)
 	m.SetBody("text/html", campaign.Content)
 
-	return d.DialAndSend(m)
+	start = time.Now()
+	err := d.DialAndSend(m)
+	duration = time.Since(start)
+	fmt.Println("Dialer created in", duration)
+
+	return err
 }
